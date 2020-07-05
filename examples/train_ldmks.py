c53a26d891d76482f43ccc004b6af2cf92d882fa
Tobias Hepp <tobias.hepp@gmail.com>
2020-07-05 20:51:24 +0000

    Training routines

--- examples/train_ldmks.py
+++ examples/train_ldmks.py
@@ -3,11 +3,10 @@ load_dotenv()
 import os
 import torch
 import numpy as np
-import importlib
 from pytorch_lightning import Trainer
 from midasmednet.dataset import MedDataset, DataReaderHDF5
 from midasmednet.unet.loss import DiceLoss
-from midasmednet.segmentation_lightning import SegmentationNet
+from midasmednet.landmarks_lightning import LandmarkNet
 from torch.utils.data import DataLoader
 import logging
 from configargparse import ArgumentParser
@@ -30,13 +29,14 @@ def _replace_env(path_str):
 # arg parsing
 parser = ArgumentParser()
 # add experiment level args
-parser.add_argument('-c', '--config', is_config_file=True, default="/mnt/share/raheppt1/data/vessel/config/aorth_seg.yaml")
+parser.add_argument('-c', '--config', is_config_file=True, default="/mnt/share/raheppt1/data/vessel/config/aorth_ldmks.yaml")
 parser.add_argument('--seed', type=int, default=0)
 parser.add_argument('--neptune_project', type=str, default='lab-midas/mednet')
-parser.add_argument('--experiment_name', type=str, default="aorth")
+parser.add_argument('--experiment_name', type=str, default="aorth_ldmks")
 parser.add_argument('--data_path', type=_replace_env)
 parser.add_argument('--image_group', type=str, default='images')
 parser.add_argument('--label_group', type=str, default='labels')
+parser.add_argument('--heatmap_group', type=str, default='heatmaps')
 parser.add_argument('--train_set', type=str)
 parser.add_argument('--val_set', type=str)
 parser.add_argument('--model_dir', type=_replace_env)
@@ -47,24 +47,23 @@ parser.add_argument('--patches_per_subject', type=int, default=10)
 parser.add_argument('--data_augmentation', action="store_true")
 parser.add_argument('--gpus', type=int, default=1)
 parser.add_argument('--preload', action='store_true')
-parser.add_argument('--resume', type=str)
 parser.add_argument('--max_epochs', type=int, default=100)
 parser.add_argument('--log_level', type=str, default='INFO')
 
 # add model specific args
-parser = SegmentationNet.add_model_specific_args(parser)
+parser = LandmarkNet.add_model_specific_args(parser)
 hparams = parser.parse_args()
 
 # set seeds
 torch.manual_seed(hparams.seed)
 torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False #True 
+torch.backends.cudnn.benchmark = True # False
 np.random.seed(hparams.seed)
 
 # logging
 # in order to use neptune logging:
 # export NEPTUNE_API_TOKEN = '...' !!! 
-logging.getLogger().setLevel(hparams.log_level)
+logging.getLogger().setLevel('INFO')
 source_files = [__file__]
 if hparams.config:
     source_files.append(hparams.config)
@@ -76,18 +75,15 @@ neptune_logger = NeptuneLogger(
     upload_source_files=source_files)
 tb_logger = loggers.TensorBoardLogger(hparams.log_dir)
 
-transform = None
-if hparams.data_augmentation:
-    transform = Compose([BrightnessTransform(mu=0.0, sigma=0.3, data_key='data'),
-                         GammaTransform(gamma_range=(0.7, 1.3), data_key='data'),
-                         ContrastAugmentationTransform(contrast_range=(0.3, 1.7), data_key='data')])
+transform = Compose([BrightnessTransform(mu=0.0, sigma=0.3, data_key='data'),
+                     GammaTransform(gamma_range=(0.7, 1.3), data_key='data'),
+                     ContrastAugmentationTransform(contrast_range=(0.3, 1.7), data_key='data')])
 
-# load validation and training key selection
-with open(hparams.train_set, "r") as keyfile:
+with open(hparams.train_set, 'r') as keyfile:
     train_keys = [l.strip() for l in keyfile.readlines()]
 print(train_keys)
 
-with open(hparams.val_set, "r") as keyfile:
+with open(hparams.val_set, 'r') as keyfile:
     val_keys = [l.strip() for l in keyfile.readlines()]
 print(val_keys)
 
@@ -97,35 +93,28 @@ train_ds = MedDataset(hparams.data_path,
                  hparams.patch_size,
                  image_group=hparams.image_group,
                  label_group=hparams.label_group,
-                 heatmap_group=None,
+                 heatmap_group=hparams.heatmap_group,
                  class_probabilities=hparams.class_probabilities,
-                 ReaderClass=DataReaderHDF5,
-                 transform=transform)
+                 transform=transform,
+                 ReaderClass=DataReaderHDF5)
 
 val_ds = MedDataset(hparams.data_path,
-                 val_keys,
+                 val_keys, 
                  hparams.patches_per_subject,
                  hparams.patch_size,
                  image_group=hparams.image_group,
                  label_group=hparams.label_group,
-                 heatmap_group=None,
+                 heatmap_group=hparams.heatmap_group,
                  class_probabilities=None,
                  ReaderClass=DataReaderHDF5)
 
-model = SegmentationNet(hparams,
-                       training_dataset=train_ds,
-                       validation_dataset=val_ds)
+model = LandmarkNet(hparams,
+                    train_ds, val_ds)
 
-# TODO add to landmarks
-kwargs = {}
-if hparams.resume:
-    print('loading checkpoint ...')
-    kwargs['resume_from_checkpoint'] = hparams.resume
 trainer = Trainer(gpus=hparams.gpus,
-                #precision=16, #amp_level='O2',
+                #precision=16, amp_level='O2',
                 max_epochs=hparams.max_epochs,
                 default_root_dir=hparams.model_dir,
-                logger=[tb_logger, neptune_logger],
-                **kwargs)
+                logger=[tb_logger, neptune_logger])
 trainer.fit(model)
 
